800b1317e8df38c4202a778525ddc9f7cfae7fac
Napwob <gorno-altaisk0@mail.ru>
2019-05-17 13:11:14 +0300

    gth

--- src/main.c
+++ src/main.c
@@ -3,39 +3,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void readin(char* h, int& x, int& y, int& r)
+struct coor {
+    int x, y, r;
+};
+
+struct coor readcr(char* h)
 {
-    int k = 0, coor;
-    char ch[100];
+    int k = 0;
+    char chx[3] = "";
+    struct coor h1;
     int i = 7;
     while (h[i] != ',') {
-        ch[k] = h[i];
+        chx[k] = h[i];
         k++;
         i++;
     }
-    x = atoi(ch);
+    h1.x = atoi(chx);
     i = i + 1;
+    char chy[3] = "";
+    k = 0;
     while (h[i] != ',') {
-        ch[k] = h[i];
+        chy[k] = h[i];
         k++;
         i++;
     }
-    y = atoi(ch);
+    h1.y = atoi(chy);
     i = i + 1;
+    char chr[3] = "";
+    k = 0;
     while (h[i] != ')') {
-        ch[k] = h[i];
+        chr[k] = h[i];
         k++;
         i++;
     }
-    r = atoi(ch);
-    i = i + 1;
+    h1.r = atoi(chr);
+    return h1;
 }
 
 int* cin()
 {
     int i = 0;
-    char h[20] = "";
-    int* a = h[];
+    char h[20] = " ";
+    int* a = h;
     char c;
     while ((c = getchar()) != '\n') {
         h[i] = c;
@@ -46,44 +55,46 @@ int* cin()
 
 int main()
 {
-    int x, y, r, errortext, errornumber, at;
-    int x1, y1, r1, errortext1, errornumber1;
-    char *h[20] = "", *h1[20] = "";
+    int errortext, errornumber, at;
+    int errortext1, errornumber1;
+    struct coor h2;
+    struct coor h0;
+    char h[20] = "", h1[20] = "";
     double s, p;
     double s1, p1;
     while (1) {
         errortext = 0;
         errornumber = 0;
         attention();
-        h = cin();
+        *h = cin();
 
         errortext1 = 0;
         errornumber1 = 0;
         attention1();
-        h1 = cin();
+        *h1 = cin();
 
         errortext = rtext(h);
         errornumber = rnumber(h);
 
         if (errortext == 0 && errornumber == 4) {
-            readin(h, x, y, r);
+            h0 = readcr(h);
         }
         errortext1 = rtext(h1);
         errornumber1 = rnumber(h1);
 
         if (errortext1 == 0 && errornumber1 == 4) {
-            readin(h1, x1, y1, r1);
+            h2 = readcr(h1);
         }
-        if ((errortext == 0 && x != 0 && y != 0 && r != 0 && errornumber == 4
-             && errortext1 == 0)
-            && (x1 != 0 && y1 != 0 && r1 != 0 && errornumber1 == 4)) {
-            s = fsp(1, r);
-            p = fsp(0, r);
-            s1 = fsp(1, r1);
-            p1 = fsp(0, r1);
-            type(x, y, r, s, p);
-            type(x1, y1, r1, s1, p1);
-            at = ati(x, y, r, x1, y1, r1);
+        if ((errortext == 0 && h0.x != 0 && h0.y != 0 && h0.r != 0
+             && errornumber == 4 && errortext1 == 0)
+            && (h2.x != 0 && h2.y != 0 && h2.r != 0 && errornumber1 == 4)) {
+            s = fsp(1, h0.r);
+            p = fsp(0, h0.r);
+            s1 = fsp(1, h2.r);
+            p1 = fsp(0, h2.r);
+            type(h0.x, h0.y, h0.r, s, p);
+            type(h2.x, h2.y, h2.r, s1, p1);
+            at = ati(h0.x, h0.y, h0.r, h2.x, h2.y, h2.r);
             if (at == 0)
                 printf("\nFigures cross each other\n");
             else
